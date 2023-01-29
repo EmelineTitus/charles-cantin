64cbc03560c06e05fb8655e467003715a440f573
Emeline <emeline.titus@hotmail.fr>
2023-01-29 18:01:00 +0100

    end

---
+++ js/scripts.js
@@ -0,0 +1,65 @@
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+window.onload = () => {
+    let filters = document.querySelectorAll("#filters div");
+
+    for(let filter of filters){
+        filter.addEventListener("click", function(){
+            let tag = this.id;
+
+            let images = document.querySelectorAll("#gallery img");
+
+            for(let image of images){
+                image.classList.replace("active", "inactive");
+
+                if(tag in image.dataset || tag === "all"){
+                    image.classList.replace("inactive", "active");
+                }
+            }
+        });
+    }
+}
